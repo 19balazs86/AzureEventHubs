0578882b9dbe523c9f66985ad72777c80bddcf9a
Balazs Kereszturi <19balazs86@gmail.com>
2024-08-19 16:15:56 +0200

    Update script: add Azurite with In-memory persistence

--- Scripts/AzuriteStart.cmd
+++ Scripts/AzuriteStart.cmd
@@ -4,4 +4,7 @@ REM https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azurite
 
 cd /d "c:\program files\microsoft visual studio\2022\community\common7\ide\extensions\microsoft\Azure Storage Emulator"
 
+REM --> In-memory persistence
+REM azurite.exe --inMemoryPersistence --skipApiVersionCheck
+
 azurite.exe --location "C:\Users\Balazs\AppData\Local\.vstools\azurite" --debug "C:\Users\Balazs\AppData\Local\.vstools\azurite\debug.log" --skipApiVersionCheck
